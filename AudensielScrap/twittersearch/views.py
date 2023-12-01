b5ef75ed082f77e254d98136f96b31c489e229fd
Ashley <koloina.andrianarivo@icloud.com>
2023-12-01 16:57:11 +0100

    ok

--- AudensielScrap/twittersearch/views.py
+++ AudensielScrap/twittersearch/views.py
@@ -8,9 +8,6 @@ from bs4 import BeautifulSoup
 import time
 from selenium.webdriver.firefox.options import Options
 import re
-from django.shortcuts import render
-from .models import TweetModele
-
 
 #installer selenium webdriver beautifulsoup4 
 # ex de requête : GET http://localhost:8000/api/search/taylor/
@@ -36,20 +33,6 @@ def get_tweets(request, mot_cle):
     # Appuie sur le bouton 
     button.click()
     
-    # # Vérification si le message de vérification apparaît
-    # try:
-    #     username_input = WebDriverWait(bot, 10).until(
-    #         EC.presence_of_element_located((By.CLASS_NAME, 'r-1yadl64')))
-        
-    #     # Si le message apparaît, remplir le champ avec le nom d'utilisateur
-    #     username = "@UserNumber59901"  # Remplacez ceci par votre nom d'utilisateur
-    #     username_input.send_keys(username)
-    #     # Cliquez sur le bouton "Suivant"
-    #     button = bot.find_element(By.CSS_SELECTOR, 'div.css-1dbjc4n.r-1m3jxhj.r-sdzlij.r-1phboty.r-rs99b7.r-19yznuf r-64el8z r-icoktb r-1ny4l3l r-1dye5f7 r-o7ynqc r-6416eg r-lrvibr')
-    #     button.click()
-    
-    # except TimeoutException:
-    #     pass
 
     # Attends que la section où écrire le mdp soit présente et y écrire le mdp
     password_input = WebDriverWait(bot, 10).until(
@@ -67,70 +50,65 @@ def get_tweets(request, mot_cle):
      # Attends que la page se charge
     time.sleep(20)
 
-    # Définir le nombre maximum de défilements 
-    max_scrolls = 5 # pb de scrolls qui peut être detecté par twitter
-    # pouvoir def la date de début et de fin de la recherche
-    
+    # Définissez le nombre maximum de défilements 
+    max_scrolls = 10  # Par exemple, 10 sections pour le défilement progressif
     scroll_count = 0
-
+    nombre_tweets = 0
     # Collecter les tweets
     tweets = []
 
-   
     while scroll_count < max_scrolls:
-        # Faire défiler la page
-        bot.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Définissez la hauteur totale de la page
+        page_height = bot.execute_script("return document.body.scrollHeight")
 
-        # Attendre un court délai pour le chargement
-        time.sleep(2)
+        # Définissez le nombre de sections que vous souhaitez
+        num_sections = 10
+        section_height = page_height // num_sections
 
-        # Extraire le contenu de la page avec BeautifulSoup
-        soup = BeautifulSoup(bot.page_source, 'html.parser') #Crée un objet BeautifulSoup à partir du code source de la page qui contient tout ce qui compose la page html
-        
-       # Trouver les tweets grâce au data-testid spécifique
-        tweet_elements = soup.find_all(attrs={'data-testid': 'tweet'})  # Use find_all to get a list of tweet elements
+        # Faites défiler chaque section à la fois
+        for i in range(num_sections):
+            bot.execute_script(f"window.scrollTo(0, {i * section_height});") # Défilement progressif de la page : 10 sections de 10% de la page à chaque fois et on récupère les tweets à chaque fois pour ne pas être bloqué par le site
+            
+            # Attendre un court délai pour le chargement
+            time.sleep(2)
 
-        # Iterate through each tweet element
-        for tweet_element in tweet_elements:
-            # On décortique les tweets pour en extraire les informations qui nous intéressent
-            # Trouver l'élément contenant le texte du tweet
-            tweet_div_text = tweet_element.find(attrs={'data-testid': 'tweetText'})
-            tweet_text = tweet_div_text.get_text(strip=True)
+            # Extraire le contenu de la page avec BeautifulSoup
+            soup = BeautifulSoup(bot.page_source, 'html.parser')
 
-            # Trouver l'élément contenant les informations sur les vues, les réponses, les likes, etc.
-            # details_tweet = tweet_element.find('div', {'aria-label': True})
+            # Trouver les tweets grâce au data-testid spécifique
+            tweet_elements = soup.find_all(attrs={'data-testid': 'tweet'})
 
-            # Convert the details_tweet element to a string
-            # details_tweet_str = str(details_tweet)
+            # Iterate through each tweet element
+            for tweet_element in tweet_elements:
+                tweet_div_text = tweet_element.find(attrs={'data-testid': 'tweetText'})
+                if tweet_div_text is not None:
+                     tweet_text = tweet_div_text.get_text(strip=True)
+                else:
+                    continue
+                details_tweet = tweet_element.find('div', {'aria-label': True})
+                details_tweet_str = str(details_tweet)
 
-            # Modèle regex pour extraire les informations (nombre + texte)
-            # pattern = r'(\d+)\s+(\w+)'
+                pattern = r'(\d+)\s+(\w+)'
+                matches = re.findall(pattern, details_tweet_str)
+                matches_str = [f"{match[0]} {match[1]}" for match in matches]
 
-            # Recherche des correspondances dans la chaîne
-            # matches = re.findall(pattern, details_tweet_str)
+                if mot_cle in tweet_text:
+                    tweets.append(tweet_text)
+                    tweets.extend(matches_str)
+                    nombre_tweets += 1
 
-            # Convert matches to strings before extending the list
-            # matches_str = [f"{match[0]} {match[1]}" for match in matches]
-
-            # Ajouter les données des tweets à la liste
-            # tweets.append(tweet_text)
-
-            # Créez une instance de TweetModele et enregistrez la chaîne dans le champ "tweet"
-            nouveau_tweet = TweetModele(tweet=tweet_text)
-            nouveau_tweet.save()
-            # tweets.extend(matches_str)
-
+        print(f"Scroll count: {scroll_count}")
         scroll_count += 1
 
+    print(f"Nombre de tweets : {nombre_tweets}")
     # Construire une réponse avec les tweets
     response_text = "\n".join(tweets)
 
     # Fermer le navigateur
     bot.quit()
-    
-   
+
     with open('twitter.html', 'w', encoding='utf-8') as f:
         f.write(soup.prettify())
-        
+
     # Retourner le texte de réponse en tant qu'objet HttpResponse
     return HttpResponse(response_text, content_type='text/plain')
