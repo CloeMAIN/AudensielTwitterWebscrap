38f08bb26dae34c764e3344cb5cdbab7ea717b9f
ClemZH <Clement.Zhuang@etu.sorbonne-universite.fr>
2024-01-02 23:17:38 +0100

    Affichage frontend non fini

--- frontend/src/Components/Recherche_tweet.js
+++ frontend/src/Components/Recherche_tweet.js
@@ -1,53 +1,145 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
-
-function SearchKeyWord({keyword}){
+function SearchKeyWord({keyword,setKeyword}){
     return(
         <form>
-            <input type="mot" placeholder="Mot Clés"/>
+            <input type="text" placeholder="Mot Clés" value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
         </form>
     );
 }
 
-function SearchBeginDate({date}){
+function SearchBeginDate({beginDate,setBeginDate}){
     return(
         <form>
-            <input type="date" placejolder="Début"/>
+            <input type="date" placeholder="Début" value={beginDate} onChange={(e) => setBeginDate(e.target.value)}/>
         </form>
     );
 }
 
 
-function SearchEndDate({date}){
+function SearchEndDate({endDate,setEndDate}){
     return(
         <form>
-            <input type="date" placejolder="Fin"/>
+            <input type="date" placeholder="Fin" value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
         </form>
     );
 }
 
-function SubmitButton({submit}){
+function SubmitButton({handleSubmit,isLoading}){
     return(
-        <button type="submit">Rechercher</button>
+        <button type="submit" onClick = {handleSubmit} disabled={isLoading}>Rechercher</button>
     );
 }
 
 
-function SearchBar({whole}){
+function SearchBar(){
+    // Definition fonction
+
+    //Definition des variables d'état
+    const [keyword, setKeyword] = useState('');
+    const [beginDate, setBeginDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [tweets, setTweets] = useState([]);
+    const [start,setStart] = useState(0);
+    const [end,setEnd] = useState(5);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+
+
+    let now = new Date();
+    //Pour avoir l'id de la requête dans le même format que dans le backend
+    let year = now.getFullYear();
+    let month = String(now.getMonth() + 1).padStart(2, '0'); // Les mois commencent à 0 en JavaScript
+    let day = String(now.getDate()).padStart(2, '0');
+    let hour = String(now.getHours()).padStart(2, '0');
+    let minute = String(now.getMinutes()).padStart(2, '0');
+    let req_id = `${year}${month}${day}${hour}${minute}`;
+    
+    
+    //Fonctions de soumission (va faire requete get_tweets par le biais url)
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        now = new Date();
+        year = now.getFullYear();
+        month = String(now.getMonth() + 1).padStart(2, '0'); // Les mois commencent à 0 en JavaScript
+        day = String(now.getDate()).padStart(2, '0');
+        hour = String(now.getHours()).padStart(2, '0');
+        minute = String(now.getMinutes()).padStart(2, '0');
+        
+        req_id = `${year}${month}${day}${hour}${minute}`;
+        // Effectuer la requête get_tweets avec keyword, beginDate et endDate
+        try{
+            const response = await axios.get(`http://localhost:8000/api/search/${keyword}/${endDate}/${beginDate}`);
+            console.log(response);
+            // On pourrait ajouter un élement montrant que la requête a été effectuée
+            // et un autre pour montrer que la requête est toujours en cours
+            // On pourrait aussi ajouter un élement montrant que la requête est terminée
+            // et un autre pour montrer que la requête a échoué
+        } catch (error){
+            console.error(error);
+
+        } finally {
+            setIsLoading(false); // Définir isLoading à false lorsque la requête est terminée
+        }
+    }
+
+    //Faire tourner cela toutes les 20 secondes
+    const handleUpdate = async (event) => {
+        event.preventDefault();
+        try{
+            const response = await axios.get(`http://localhost:8000/api/search_new/${req_id}`);
+            setTweets(response.data);
+        }
+        catch (error){
+            console.error(error);
+        }
+    }
+
+
+    const handleNext = () => {
+        setStart(oldStart =>oldStart+5);
+        setEnd(oldEnd=>oldEnd+5);
+    }
+
+    const handlePrevious = () => {
+        setStart(oldStart => oldStart-5);
+        setEnd(oldEnd => oldEnd-5);
+    }
+
+
+    //Affichage 
     return(
-    <tr>
-        <td>
-            <SearchKeyWord/>
-        </td>
-        <td>
-            <SearchBeginDate/>
-        </td>
-        <td>
-            <SearchEndDate/>
-        </td>
-        <td>
-            <SubmitButton/>
-        </td>
-    </tr>
+        <div className="FormsAndTweet">
+
+            <form onSubmit={handleSubmit}>
+                <SearchKeyWord keyword={keyword} setKeyword={setKeyword}/>
+                <SearchBeginDate beginDate={beginDate} setBeginDate={setBeginDate}/>
+                <SearchEndDate endDate={endDate} setEndDate={setEndDate}/> 
+                <SubmitButton handleSubmit={handleSubmit} isLoading={isLoading}/> 
+                {/*On doit add la fonction handleUpdate si handleSubmit pressed
+                Soit on utilise un useState de submitPressed,setSubmitPressed
+                pour déterminer quand arrêter de faire la boucle de handleUpdate*/}
+            </form>
+
+            <div className = "TweetTable">
+                <button onClick = {handlePrevious}>Previous</button>
+                <button onClick = {handleNext}>Next</button>
+                <button onClick={handleUpdate}>Update</button>
+
+                <div className = "TableElement">
+                    {tweets.slice(start, end).map((tweet, index) => (
+                        <div key={index}>
+                            <p>{tweet.content}</p>
+                            <p>Views: {tweet.views}</p>
+                            <p>Likes: {tweet.likes}</p>
+                            <p>Replies: {tweet.replies}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 }
 
