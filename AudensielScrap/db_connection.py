418811f2eacbfe131029aad51a7004da7ab8dd1c
Ashley <koloina.andrianarivo@icloud.com>
2024-03-27 20:51:46 +0100

    amélioration visuel

--- AudensielScrap/db_connection.py
+++ AudensielScrap/db_connection.py
@@ -1,7 +1,7 @@
 import pymongo
 
 # Connexion à la base de données MongoDB à l'aide du nom du service MongoDB défini dans Docker Compose
-client = pymongo.MongoClient("mongodb://mongo:27017/")
+client = pymongo.MongoClient("mongodb+srv://cloe:Webscrap23@cluster0.qnvy73r.mongodb.net/?authMechanism=SCRAM-SHA-1")
 
 # Sélection de la base de données
 db = client['Tweets']
